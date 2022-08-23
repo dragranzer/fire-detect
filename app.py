32312ba65df2f89fb1256c05de979b51a53bd755
ivannizar <ivannizar@gmail.com>
2022-08-23 18:02:57 +0700

    fix local ip

--- app.py
+++ app.py
@@ -70,4 +70,4 @@ def clasify_image():
         }), 200
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    app.run(debug=True, host="127.0.0.1", port=int(os.environ.get("PORT", 8080)))
